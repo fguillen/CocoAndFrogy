bfa82d8da33ca9101d9dd5004d04343ffbf2a345
Fernando Guillen <fguillen.mail@gmail.com>
2023-04-27 20:19:36 +0200

    Animation tres on brick

--- actors/bricks/brick.gd
+++ actors/bricks/brick.gd
@@ -7,6 +7,9 @@ extends StaticBody2D
 # -- 04 # docstring
 #
 # -- 05 signals
+signal hurt()
+signal died()
+
 # -- 06 enums
 # -- 07 constants
 # -- 08 exported variables
@@ -16,6 +19,9 @@ extends StaticBody2D
 # -- 09 public variables
 # -- 10 private variables
 # -- 11 onready variables
+@onready var animation_player = $AnimationPlayer
+@onready var animation_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
+
 #
 # -- 12 optional built-in virtual _init method
 # -- 13 optional built-in virtual _enter_tree() method
@@ -23,13 +29,17 @@ extends StaticBody2D
 # -- 15 remaining built-in virtual methods
 # -- 16 public methods
 func impact():
+	hurt.emit()
 	health -= 1
+	animation_state_machine.travel("hurt")
+	
 	if health <= 0:
 		_die()
 	
 		
 # -- 17 private methods
 func _die():
+	died.emit()
 	queue_free()
 	
 # -- 18 signal listeners
