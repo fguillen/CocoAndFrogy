ec5a64eb13ff028510a87f2282a5f6e63ea414e9
Fernando Guillen <fguillen.mail@gmail.com>
2023-05-26 00:47:56 +0200

    DeformEffect with shader

--- actors/coco/effects_manager.gd
+++ actors/coco/effects_manager.gd
@@ -1,30 +1,34 @@
 # -- 01 @tool
 # -- 02 class_name
-class_name Coco
 # -- 03 extends
-extends Character
+extends Node2D
 
 # -- 04 # docstring
 #
 # -- 05 signals
+signal impact_frogy_coco_ocurred()
+
 # -- 06 enums
 # -- 07 constants
 # -- 08 exported variables
 # -- 09 public variables
 # -- 10 private variables
 # -- 11 onready variables
-@onready var frogy_handler: CanvasItem = $FrogyHandler
-
 #
 # -- 12 optional built-in virtual _init method
 # -- 13 optional built-in virtual _enter_tree() method
 # -- 14 built-in virtual _ready method
 func _ready():
-	Global.coco_ready(self)
+	GlobalEvents.impact_with_coco_occurred.connect(_on_impact_frogy_coco)
 	
 	
 # -- 15 remaining built-in virtual methods
 # -- 16 public methods
 # -- 17 private methods
+func _on_impact_frogy_coco():
+	impact_frogy_coco_ocurred.emit()
+	
+	
 # -- 18 signal listeners
 # -- 19 subclasses
+
