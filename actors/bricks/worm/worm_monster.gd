340c29ff057f8e6cc610bd3b6a36ef9ba20e1579
Fernando Guillen <fguillen.mail@gmail.com>
2023-06-06 14:32:27 +0200

    WormMonster (II)

--- actors/bricks/worm/worm_monster.gd
+++ actors/bricks/worm/worm_monster.gd
@@ -13,15 +13,16 @@ extends Node2D
 @export var head: Node2D
 @export var body: Node2D
 @export var speed := 100.0
-@export var bricks_distance := 200.0
+@export var bricks_distance := 100.0
 
 @export_group("Path")
 @export var path: Path2D
+@export var path_follow_template: PathFollow2D
 
 
 # -- 09 public variables
 # -- 10 private variables
-var _moving_bricks: Array[MovingBrick]
+var _moving_body_parts: Array[MovingBodyPart]
 var _actual_position := 0.0
 
 # -- 11 onready variables
@@ -30,46 +31,59 @@ var _actual_position := 0.0
 # -- 13 optional built-in virtual _enter_tree() method
 # -- 14 built-in virtual _ready method
 func _ready():
-	_init_moving_bricks()
+	_init_moving_body_parts()
 	
 	
 # -- 15 remaining built-in virtual methods
 func _physics_process(delta):
-	_move_moving_bricks(delta)
+	_move_moving_body_parts(delta)
 	
 	
 # -- 16 public methods
 # -- 17 private methods
-func _init_moving_bricks():
+func _init_moving_body_parts():
 	var path_follow_position := 0.0
 	
-	for brick in body.get_children():
-		var path_follow = PathFollow2D.new()
-		path.add_child(path_follow)
-		path_follow.progress = path_follow_position
-		path_follow.rotates = false
-		
-		var moving_brick = MovingBrick.new(brick, path_follow)
+	_init_moving_body_part(head, path_follow_position)
+	path_follow_position += bricks_distance
+	
+	for body_part in body.get_children():
+		_init_moving_body_part(body_part, path_follow_position)
 		path_follow_position += bricks_distance
-		
-		_moving_bricks.append(moving_brick)
-		
 	
-func _move_moving_bricks(delta):
-	for moving_brick in _moving_bricks:
-		moving_brick.path_follow.progress += speed * delta
-		var brick_direction = moving_brick.brick.global_position.direction_to(moving_brick.path_follow.global_position)
-		moving_brick.brick.move_and_collide(brick_direction * speed)
-		
 	
+func _init_moving_body_part(body_part: PhysicsBody2D, path_follow_position: float):
+	var path_follow = path_follow_template.duplicate()
+	path.add_child(path_follow)
+	path_follow.progress = path_follow_position
+	body_part.reparent(path_follow)
+	body_part.global_position = path_follow.global_position
+	
+	var moving_body_part = MovingBodyPart.new(body_part, path_follow)
+	path_follow_position += bricks_distance
+	
+	_moving_body_parts.append(moving_body_part)
+	
+	
+func _move_moving_body_parts(delta):
+	print("XXX: move_moving_body_parts")
+	for moving_body_part in _moving_body_parts:
+		moving_body_part.path_follow.progress += speed * delta
+#		var distance = moving_body_part.body_part.global_position.distance_to(moving_body_part.path_follow.global_position)
+#		if distance > 2.0: 
+#			var brick_direction = moving_body_part.body_part.global_position.direction_to(moving_body_part.path_follow.global_position)
+#			print("XXX: brick_direction: " , brick_direction)
+#			moving_body_part.body_part.move_and_collide(brick_direction * 100 * delta)
+#
+#
 # -- 18 signal listeners
 # -- 19 innerclasses
 
-class MovingBrick:
-	var brick: Brick
+class MovingBodyPart:
+	var body_part: PhysicsBody2D
 	var path_follow: PathFollow2D
 	
-	func _init(brick: Brick, path_follow: PathFollow2D):
-		self.brick = brick
+	func _init(body_part: PhysicsBody2D, path_follow: PathFollow2D):
+		self.body_part = body_part
 		self.path_follow = path_follow
 
