662c25d6aed57df624cc5781b29ccb89fe278201
Fernando Guillen <fguillen.mail@gmail.com>
2023-06-09 22:23:14 +0200

    LevelSelection Menu

---
+++ ui/level_selection_button.gd
@@ -0,0 +1,32 @@
+# -- 01 @tool
+# -- 02 class_name
+class_name LevelSelectionButton
+# -- 03 extends
+extends Control
+
+# -- 04 # docstring
+#
+# -- 05 signals
+# -- 06 enums
+# -- 07 constants
+# -- 08 exported variables
+# -- 09 public variables
+# -- 10 private variables
+# -- 11 onready variables
+#
+# -- 12 optional built-in virtual _init method
+# -- 13 optional built-in virtual _enter_tree() method
+# -- 14 built-in virtual _ready method
+# -- 15 remaining built-in virtual methods
+func _input(event):
+	pass
+# -- 16 public methods
+# -- 17 private methods
+# -- 18 signal listeners
+func _on_gui_input(event):
+	print("XXX: _on_gui_input(%s)" % event)
+	
+	
+	
+# -- 19 innerclasses
+
